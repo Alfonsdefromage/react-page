223f78b300894d59f9d78fee93f8cfa59778cc4b
Igor Zapala <zapala.igor@gmail.com>
2025-05-11 21:37:38 +0900

    I've created an about page

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,18 @@
-import { useState } from "react";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import About from "./pages/About";
+import SidebarLayout from "./layouts/SidebarLayout";
 import "./App.css";
 
 function App() {
   return (
     <>
-      <div>Let's start</div>
+      <Router>
+        <Routes>
+          <Route element={<SidebarLayout />}>
+            <Route path="/" element={<About />} />
+          </Route>
+        </Routes>
+      </Router>
     </>
   );
 }
